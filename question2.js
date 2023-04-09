7ada684235be39817ae4b8c6ac8613da475742e2
josiemaylinn <124173145+josiemaylinn@users.noreply.github.com>
2023-04-09 16:23:13 -0400

    Create question2.js

---
+++ question2.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0);
+
+  for (let starX = 0; starX < 1000; starX += 125) {
+    for (let starY = 0; starY < 1000; starY += 125) {
+      drawStar("gold", starX, starY, 6);
+    }
+  }
+}
+
+function drawStar(color, x, y, points) {
+  push();
+  translate(x, y);
+
+  for (let numberOfPoints = 0; numberOfPoints < points; numberOfPoints++) {
+    rotate(360 / points);
+    noStroke();
+    fill(color);
+    triangle(0, 0, -25, 50, -25, -50);
+  }
+  pop();
+}
